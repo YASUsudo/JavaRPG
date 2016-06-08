958db7301888bef2ad5fcbc5085c9c2372cc9ad0
m-yatsuzuka <m-yatsuzuka@bgtpc191.begood-tech.com>
2016-06-08 14:35:14 +0900

    味方をリスト化

--- src/com/katolisa/rpg/Main.java
+++ src/com/katolisa/rpg/Main.java
@@ -1,4 +1,5 @@
 package com.katolisa.rpg;
+import java.util.ArrayList;
 
 public class Main {
 	public static final int A = 0;
@@ -8,24 +9,29 @@ public class Main {
 
 	public static void main(String[] args) {
 		int turn = 0;
+		ArrayList<Human> characters = new ArrayList<Human>();
 
 		Hero hero = new Hero("コマドリ", 100, A);
 		System.out.println("勇者を生成しました");
 		System.out.println("名前：" + hero.getName() + "、HP：" + hero.getHp() + "、血液型：" + hero.getblood());
+		characters.add(hero);
 
 		Wizard wizard = new Wizard("マドカ", 50, A);
 		System.out.println("魔法使いを生成しました");
 		System.out.println("名前：" + wizard.getName() + "、HP：" + wizard.getHp() + "、血液型：" + wizard.getblood());
+		characters.add(wizard);
 
 		Boss boss = new Boss("ムテキ", 10000, AB);
 		System.out.println("ボスを生成しました");
 		System.out.println("名前：" + boss.getName() + "、HP：" + boss.getHp() + "、血液型：" + boss.getblood());
 
-		while(turn < 3) {
+		while(turn < 10) {
 			turn++;
 			System.out.println("========== " + turn + "ターン目 ==========");
-			hero.attack(boss);
-			wizard.attack(boss);
+			for (Human h : characters) {
+				h.attack(boss);
+			}
+
 			if (boss.getHp() <= 0) {
 				System.out.println(boss.getName() + "を倒した！");
 				break;
@@ -34,13 +40,21 @@ public class Main {
 			if (turn % 2 == 0) {
 				boss.defend();
 			} else {
-				boss.attack(hero);
+				boss.attack(characters.get(0));
 			}
-			if (hero.getHp() <= 0) {
+
+			if (characters.contains(hero) && hero.getHp() <= 0) {
 				System.out.println("勇者" + hero.getName() + "は力尽きた");
+				characters.remove(hero);
 			}
-			if (wizard.getHp() <= 0) {
+			if (characters.contains(wizard) && wizard.getHp() <= 0) {
 				System.out.println("魔法使い" + wizard.getName() + "は力尽きた");
+				characters.remove(wizard);
+			}
+
+			if (characters.isEmpty()) {
+				System.out.println("味方が全滅した…");
+				break;
 			}
 
 			System.out.println("ボスHP：" + boss.getHp() + "勇者HP：" + hero.getHp() + "魔法使いHP：" + wizard.getHp());
