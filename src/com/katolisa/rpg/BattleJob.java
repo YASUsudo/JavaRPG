06cd4d218c8cea1c6d6d091d803ddb23eef6eda3
katolisa <maho.yatsuzuka@stylez.co.jp>
2016-06-08 19:14:11 +0900

    BattleJobからBattleServiceの呼び出し

--- src/com/katolisa/rpg/BattleJob.java
+++ src/com/katolisa/rpg/BattleJob.java
@@ -8,6 +8,8 @@ import com.katolisa.rpg.character.Character;
 import com.katolisa.rpg.character.impl.Boss;
 import com.katolisa.rpg.character.impl.Hero;
 import com.katolisa.rpg.character.impl.Wizard;
+import com.katolisa.rpg.service.BattleService;
+import com.katolisa.rpg.service.impl.BattleServiceImpl;
 
 public class BattleJob {
 
@@ -35,51 +37,10 @@ public class BattleJob {
 		System.out.printf("名前：%s、HP：%d、血液型：%d\n\n", boss.getName(), boss.getHp(), boss.getBlood());
 
 		if (command == ATTACK) {
-			// 攻撃バトル
+			BattleService battleService = new BattleServiceImpl();
+				battleService.attack(party, boss);
 		} else {
 			// 例外
 		}
-
-		int turn = 0;
-
-		// 攻撃コマンドパターン
-		while(turn < 10) {
-			turn++;
-			System.out.println("========== " + turn + "ターン目 ==========");
-
-			// パーティの行動
-			for (Character character : party) {
-				character.attack(boss);
-			}
-
-			if (boss.getHp() <= 0) {
-				System.out.println(boss.getName() + "を倒した！");
-				break;
-			}
-
-			// ボスの行動
-			if (turn % 2 == 0) {
-				boss.defend();
-			} else {
-				// 攻撃対象の選定
-				int r = new java.util.Random().nextInt(party.size());
-				Character target = party.get(r);
-
-				boss.attack(target);
-				if (target.getHp() <= 0) {
-					System.out.println(target.getName() + "は力尽きた");
-					party.remove(target);
-				}
-			}
-
-			if (party.isEmpty()) {
-				System.out.println("味方が全滅した…");
-				break;
-			}
-
-			System.out.printf("\nボスHP：%d、勇者HP：%d、魔法使いHP：%d\n\n", boss.getHp(), hero.getHp(), wizard.getHp());
-		}
-		System.out.println("おわり");
 	}
-
 }
